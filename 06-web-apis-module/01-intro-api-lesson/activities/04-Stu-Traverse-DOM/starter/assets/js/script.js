02d7c40098854b0e1860602781279f6290032979
Adrian Tut <adriantutuianu01@gmail.com>
2023-12-19 20:08:20 +0000

    Setting attributes task

--- 06-web-apis-module/01-intro-api-lesson/activities/04-Stu-Traverse-DOM/starter/assets/js/script.js
+++ 06-web-apis-module/01-intro-api-lesson/activities/04-Stu-Traverse-DOM/starter/assets/js/script.js
@@ -6,4 +6,4 @@ console.log(headerDiv.children);
 
 // Change style by accessing style object's properties
 headerDiv.children[0].style.color = "white";
-articlesDiv.children[0].children[1].style.fontSize = "50px";
+articlesDiv.children[0].style.fontSize = "50px";
