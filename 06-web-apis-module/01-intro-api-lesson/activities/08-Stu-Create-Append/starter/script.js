21ae812ba6cc056d5475d20b81b253caaade435b
Adrian Tut <adriantutuianu01@gmail.com>
2023-12-19 20:50:33 +0000

    Create append task

--- 06-web-apis-module/01-intro-api-lesson/activities/08-Stu-Create-Append/starter/script.js
+++ 06-web-apis-module/01-intro-api-lesson/activities/08-Stu-Create-Append/starter/script.js
@@ -18,6 +18,11 @@ kittenEl.textContent = "This is my kitten 🐱.";
 nameEl.textContent = "His name is Jax.";
 favoriteEl.textContent = "My favorite foods are:";
 
+li1.textContent = "pasta";
+li2.textContent = "fires";
+li3.textContent = "burger";
+li4.textContent = "pizza";
+
 body.appendChild(h1El);
 body.appendChild(infoEl);
 infoEl.appendChild(imgEl);
@@ -25,7 +30,7 @@ infoEl.appendChild(kittenEl);
 infoEl.appendChild(nameEl);
 body.appendChild(favoriteEl);
 favoriteEl.appendChild(listEl);
-// Append ordered list 
+// Append ordered list
 favoriteEl.appendChild(listEl);
 
 h1El.setAttribute("style", "margin:auto; width:50%; text-align:center;");
@@ -36,3 +41,14 @@ kittenEl.setAttribute("style", "font-size:25px; text-align:center;");
 favoriteEl.setAttribute("style", "font-size:20px;");
 
 // TODO: Add ordered list items containing four favorite foods
+body.appendChild(favoriteEl);
+listEl.appendChild(li1);
+listEl.appendChild(li2);
+listEl.appendChild(li3);
+listEl.appendChild(li4);
+
+listEl.setAttribute("style", "background-color: #333333; padding: 20px;");
+li1.setAttribute("style", "background-color: blue;");
+li2.setAttribute("style", "background-color: green;");
+li3.setAttribute("style", "background-color: yellow;");
+li4.setAttribute("style", "background-color: pink;");
