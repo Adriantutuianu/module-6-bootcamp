3a85a11a177ff49c95336272dbcac5d348b70a0e
Adrian Tut <adriantutuianu01@gmail.com>
2023-12-20 21:17:48 +0000

    Data attributes task

--- 06-web-apis-module/02-js-events-lesson/activities/10-Stu-Data-Attributes/starter/assets/js/script.js
+++ 06-web-apis-module/02-js-events-lesson/activities/10-Stu-Data-Attributes/starter/assets/js/script.js
@@ -1,7 +1,19 @@
 const container = document.querySelector(".container");
 
-container.addEventListener("click", function(event) {
+container.addEventListener("click", function (event) {
   const element = event.target;
 
   // TODO: Complete function
+  if (element.matches(".box")) {
+    var state = element.getAttribute("data-state");
+
+    if (state === "hidden") {
+      //get number from data-number and we set it as the text content for the current box
+      element.textContent = element.dataset.number;
+      element.dataset.state = "visible";
+    } else {
+      element.textContent = "";
+      element.setAttribute("data-state", "hidden");
+    }
+  }
 });
