a4d746df38068736001c9a603b8a2486385921b0
Adrian Tut <adriantutuianu01@gmail.com>
2023-12-21 19:15:02 +0000

    Local storage task

--- 06-web-apis-module/03-client-side-storage-lesson/activities/02-Stu-Local-Storage-User/starter/script.js
+++ 06-web-apis-module/03-client-side-storage-lesson/activities/02-Stu-Local-Storage-User/starter/script.js
@@ -15,12 +15,22 @@ function displayMessage(type, message) {
 function renderLastRegistered() {
   // Fill in code here to retrieve the last email and password.
   // If they are null, return early from this function
-  // Else set the text of the userEmailSpan and userPasswordSpan 
+  // Else set the text of the userEmailSpan and userPasswordSpan
   // to the corresponding values form local storage
-  
+  var lastEmail = localStorage.getItem("email");
+  if (!lastEmail) {
+    return;
+  }
+  userEmailSpan.textContent = lastEmail;
+
+  var lastPassword = localStorage.getItem("password");
+  if (!lastPassword) {
+    return;
+  }
+  userPasswordSpan.textContent = lastPassword;
 }
 
-signUpButton.addEventListener("click", function(event) {
+signUpButton.addEventListener("click", function (event) {
   event.preventDefault();
 
   const email = document.querySelector("#email").value;
@@ -33,6 +43,10 @@ signUpButton.addEventListener("click", function(event) {
   } else {
     displayMessage("success", "Registered successfully");
 
-  // Save email and password to localStorage and render the last registered.
+    // Save email and password to localStorage and render the last registered.
+
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
+    renderLastRegistered();
   }
 });
